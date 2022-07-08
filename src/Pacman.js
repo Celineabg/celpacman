fb274bccd76fb9c80bd4e2a9a76c9fc4b4b4e48d
TeodorNTenev <t.tenev.zklas@gmail.com>
2022-07-08 11:41:32 +0300

    commitfinal

--- src/Pacman.js
+++ src/Pacman.js
@@ -23,6 +23,7 @@ export default class Pacman {
     this.madeFirstMove = false;
 
     document.addEventListener("keydown", this.#keydown);
+    //document.addEventListener("keyup", this.#keyup);
 
     this.#loadPacmanImages();
   }
@@ -112,6 +113,36 @@ export default class Pacman {
       this.madeFirstMove = true;
     }
   };
+  /* #keyup = (event) => {
+    //up
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down)
+        this.currentMovingDirection = MovingDirection.up;
+      this.requestedMovingDirection = MovingDirection.up;
+      this.madeFirstMove = false;
+    }
+    //down
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up)
+        this.currentMovingDirection = MovingDirection.down;
+      this.requestedMovingDirection = MovingDirection.down;
+      this.madeFirstMove = false;
+    }
+    //left
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right)
+        this.currentMovingDirection = MovingDirection.left;
+      this.requestedMovingDirection = MovingDirection.left;
+      this.madeFirstMove = false;
+    }
+    //right
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left)
+        this.currentMovingDirection = MovingDirection.right;
+      this.requestedMovingDirection = MovingDirection.right;
+      this.madeFirstMove = false;
+    }
+  }; */
 
   #move() {
     if (this.currentMovingDirection !== this.requestedMovingDirection) {
